26963161a393017a31b3367a2d70b7ae6839e90c
sonhero981 <sonhero981@gmail.com>
2022-06-09 20:03:26 +0700

    update

--- modules/auth/auth.router.js
+++ modules/auth/auth.router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("./auth.controller");
 const validateInput = require("../middleware/validateInput");
+const needAuthenticated = require("../middleware/needAuthenticated");
 const { loginSchema, registerSchema } = require("./auth.validation");
 
 router.post(
