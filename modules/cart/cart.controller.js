a50292d565ffb6d8a25d9e9dc5582ba029787a27
sonhero981 <sonhero981@gmail.com>
2022-06-10 08:56:35 +0700

    updateCart

--- modules/cart/cart.controller.js
+++ modules/cart/cart.controller.js
@@ -1,3 +1,4 @@
+const HTTPError = require("../common/httpError");
 const CartModel = require("./cart");
 
 // const createCart = async (req, res, next) => {
@@ -57,7 +58,14 @@ const createOrUpdateCart = async (req, res, next) => {
 
 const getCartByUser = async (req, res, next) => {
   const senderUser = req.user;
+
+  if (!senderUser) {
+    throw new HTTPError(401, "Chưa đăng nhập");
+  }
   const foundCart = await CartModel.findOne({ createdBy: senderUser._id });
+  if (!foundCart) {
+    throw new HTTPError(401, "Chưa tạo giỏ hàng");
+  }
   res.send({ success: 1, data: foundCart });
 };
 
